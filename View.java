604fc1523e6e782cbf928e01f3e969429744cd73
NhatHoang26122003 <116450851+NhatHoang26122003@users.noreply.github.com>
2023-09-21 21:28:45 +0700

    Add files via upload

---
+++ View.java
@@ -0,0 +1,25 @@
+package BT;
+
+import java.awt.Color;
+
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.border.Border;
+
+public class View extends JFrame {
+	public static void main(String[] args) {
+		new View ();
+	}
+	public View() {
+		this.setSize(600,400);
+		this.setTitle("Short Path Routing");
+		this.setDefaultCloseOperation(3);
+		this.setLocationRelativeTo(null);
+		JRadio btn_1;
+		btn_1 = new JButton ("Chọn");
+		Border borderTiltle = BorderFactory.createLineBorder(Color.BLUE);
+		
+		
+		this.setVisible(true);
+	}
+}
